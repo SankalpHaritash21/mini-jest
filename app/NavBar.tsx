def08ab05c3b027885d0b020bf14d3288d0c3dd6
Sankalp <sanklpkaushik21@gmail.com>
2024-02-22 17:19:42 +0530

    Style Navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,23 +1,32 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues/list" },
+    { label: "Issues", href: "/issues" },
   ];
 
   return (
-    <nav className="flex items-center justify-evenly h-14 mb-5 px-5 select-none">
-      <Link href="/">
+    <nav className="flex items-center justify-evenly h-14 mb-5 px-5 select-none border-b-black border-b-2">
+      <Link href="/" className="w-1/2 px-4">
         <AiFillBug />
       </Link>
-      <ul className="flex items-center space-x-20">
+      <ul className="flex items-center space-x-20 w-1/2 justify-start text-2xl font-bold">
         {links.map((link) => (
           <li key={link.href}>
             <Link
-              className={"text-zinc-500 hover:text-zinc-900"}
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
               href={link.href}
             >
               {link.label}
@@ -25,7 +34,6 @@ const NavBar = () => {
           </li>
         ))}
       </ul>
-      <div></div>
     </nav>
   );
 };
